6a037c344263b84e000226101aff039f488a2ded
zzossig <zzossig@gmail.com>
2020-11-03 18:36:05 +0900

    circular percentage complete

--- lib/screens/result.dart
+++ lib/screens/result.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:death_counter/screens/intro.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:death_counter/widgets/radial_painter.dart';
 
 class ResultScreen extends StatefulWidget {
   @override
@@ -11,6 +12,7 @@ class ResultScreen extends StatefulWidget {
 class _ResultScreenState extends State<ResultScreen> {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   int _remainingDays;
+  double _remainingPerc;
   DateTime _today = new DateTime.now();
   DateTime _todayEnd;
   Duration _remainingTime;
@@ -38,12 +40,17 @@ class _ResultScreenState extends State<ResultScreen> {
 
     var now = new DateTime.now();
     var endDate = DateTime(
-        registeredYear + (lifeSpan - curAge), DateTime.july, 1, 00, 00, 1, 1);
+        registeredYear + (lifeSpan - curAge), DateTime.july, 1, 0, 0, 1, 1);
+    var birthDate =
+        DateTime(registeredYear - curAge, DateTime.july, 1, 0, 0, 1, 1);
     var diff = endDate.difference(now);
+    var passedDays = now.difference(birthDate).inDays;
     var remainingDays = diff.inDays - 1; // -1 for countdown time
+    var remainingPerc = passedDays / (remainingDays + passedDays);
 
     setState(() {
       _remainingDays = remainingDays;
+      _remainingPerc = remainingPerc;
     });
   }
 
@@ -134,75 +141,118 @@ class _ResultScreenState extends State<ResultScreen> {
                 );
               },
             ),
+            ListTile(
+              title: Text('판단 근거'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => IntroScreen(),
+                  ),
+                );
+              },
+            ),
           ],
         ),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        // crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Stack(
-                overflow: Overflow.visible,
-                children: [
-                  Container(
-                    width: 200,
-                    height: 200,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          "${times[0]}시 ${times[1]}분 ${times[2].substring(0, 6)}초",
+          Container(
+            child: Text('${(_remainingPerc * 100).toStringAsFixed(2)}%',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.red,
+                )),
+          ),
+          SizedBox(
+            height: 12.0,
+          ),
+          CustomPaint(
+            size: Size(300, 300),
+            foregroundPainter: RadialPainter(
+              bgColor: Colors.grey,
+              lineColor: Colors.red,
+              percent: _remainingPerc,
+              width: 4.0,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Stack(
+                  overflow: Overflow.visible,
+                  children: [
+                    Container(
+                      width: 200,
+                      height: 200,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "${times[0]}시 ${times[1]}분 ${times[2].substring(0, 6)}초",
+                            style: TextStyle(
+                              fontSize: 18.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 2),
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.4),
+                            spreadRadius: 7,
+                            blurRadius: 7,
+                            offset: Offset(0, 0),
+                          ),
+                        ],
+                        gradient: LinearGradient(
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                          colors: [
+                            Colors.black.withOpacity(0.9),
+                            Colors.black87.withOpacity(0.5),
+                            Colors.black54.withOpacity(0.7),
+                            Colors.black38.withOpacity(0.9),
+                          ],
+                          stops: [0.1, 0.4, 0.6, 0.9],
+                        ),
+                        // backgroundBlendMode:
+                      ),
+                    ),
+                    Positioned.fill(
+                      top: -50,
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "$_remainingDays 일",
                           style: TextStyle(
-                            fontSize: 18.0,
+                            fontSize: 10.0,
                           ),
                         ),
-                      ],
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey, width: 2),
-                      shape: BoxShape.circle,
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.4),
-                          spreadRadius: 2,
-                          blurRadius: 7,
-                          offset: Offset(0, 0),
-                        ),
-                      ],
-                      gradient: LinearGradient(
-                        begin: Alignment.topRight,
-                        end: Alignment.bottomLeft,
-                        colors: [
-                          Colors.black.withOpacity(0.9),
-                          Colors.black87.withOpacity(0.5),
-                          Colors.black54.withOpacity(0.7),
-                          Colors.black38.withOpacity(0.9),
-                        ],
-                        stops: [0.1, 0.4, 0.6, 0.9],
                       ),
-                      // backgroundBlendMode:
                     ),
-                  ),
-                  Positioned.fill(
-                    top: -50,
-                    child: Align(
-                      alignment: Alignment.center,
-                      child: Text(
-                        "$_remainingDays 일",
-                        style: TextStyle(
-                          fontSize: 10.0,
+                    Positioned.fill(
+                      top: 50,
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          "남았습니다",
+                          style: TextStyle(
+                            fontSize: 10.0,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          )
+                  ],
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
